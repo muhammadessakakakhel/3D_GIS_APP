9b014b5ebf1409a8061ee8dde937a36898e393a5
muhammadessakakakhel <muhammadessakakakhel966@gmail.com>
2024-11-01 11:28:36 +0500

    Updated code

--- run_first/vite.config.ts
+++ run_first/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001, // Set your preferred port here
+    port: 3002, // Set your preferred port here
   },
 });
--- src/components/PotreeView.tsx
+++ src/components/PotreeView.tsx
@@ -6,7 +6,7 @@ const PotreeViewer = () => {
   return (
     <div>
       <iframe 
-        src="http://localhost:3001/" // URL of the standalone Potree app
+        src="http://localhost:3002/" // URL of the standalone Potree app
         style={{ width: '100%', height: '500px', border: 'none' }}
         title="3D Viewer"
       />
